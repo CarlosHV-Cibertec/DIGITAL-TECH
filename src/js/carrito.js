46a8134f4e4cbd354adf2cab77c29a76515d445d
luispal7030 <i202224764@cibertec.edu.pe>
2023-06-20 18:19:29 -0500

    subida de mi versionado, en otro git

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,4 +1,4 @@
-const carOff = document.getElementById('carrito');
+const carOff = document.getElementById("carrito");
 
 carOff.style.display = "none";
 
@@ -6,60 +6,54 @@ carOff.style.display = "none";
 function toggleDiv(divId) {
   var div = document.getElementById(divId);
   if (div.style.display === "none") {
-    div.style.display = "block";
+    div.style.display = "flex";
   } else {
     div.style.display = "none";
   }
 }
 
-
-
 // * logica de carrito de compras
 
+//variables del carrito
+// const carritoContn = document.getElementById("contenedor");
+// const carrito = [];
+// var cantProdAdd = 0;
+// var precioTotal = 0;
+// Función para agregar un celular al carrito (ejemplo de implementación)
+// function agregarAlCarrito(ruta, marca, modelo, precio) {
 
-  //variables del carrito
-  // const carritoContn = document.getElementById("contenedor");
-  // const carrito = [];
-  // var cantProdAdd = 0;
-  // var precioTotal = 0;
-  // Función para agregar un celular al carrito (ejemplo de implementación)
-  // function agregarAlCarrito(ruta, marca, modelo, precio) {
+// Generar un identificador único para el producto
+// const prodSelect = {
+//   id: setIdUnico(),
+//   marca: marca,
+//   modelo: modelo,
+//   imagen: ruta,
+//   precio: precio,
+// };
 
-     // Generar un identificador único para el producto
-    // const prodSelect = {
-    //   id: setIdUnico(),
-    //   marca: marca,
-    //   modelo: modelo,
-    //   imagen: ruta,
-    //   precio: precio,
-    // };
+//añadir diseño de producto seleccionado a carrito
+//   const addproductHTML =
+//       `
+//       <div class="producto">
+//       <div class="imagenprod">
+//         <img src="${ruta}" alt="${marca} ${modelo}">
+//       </div>
+//       <div class="descripcion">
+//           <h4>${marca} ${modelo}</h4>
+//           <p>${precio} $</p>
+//       </div>
+//       <div class="removeProd">
+//           <p>x</p>
+//       </div>
+//     </div>`;
 
-    //añadir diseño de producto seleccionado a carrito
-  //   const addproductHTML = 
-  //       `
-  //       <div class="producto"> 
-  //       <div class="imagenprod">
-  //         <img src="${ruta}" alt="${marca} ${modelo}">
-  //       </div>
-  //       <div class="descripcion">
-  //           <h4>${marca} ${modelo}</h4>
-  //           <p>${precio} $</p>
-  //       </div>
-  //       <div class="removeProd"> 
-  //           <p>x</p>
-  //       </div>
-  //     </div>`;
+// precioTotal += precio;
+// carritoContn.innerHTML += addproductHTML;
+// carrito.push(prodSelect);
+//   // Implementa tu lógica para agregar el celular al carrito
+//   console.log(carrito);
+// }
 
-  // precioTotal += precio;
-  // carritoContn.innerHTML += addproductHTML;
-  // carrito.push(prodSelect);
-  //   // Implementa tu lógica para agregar el celular al carrito
-  //   console.log(carrito);
-  // }
-  
-
-
-  // function setIdUnico (){
-  //   return cantProdAdd++;
-  // }
-  
+// function setIdUnico (){
+//   return cantProdAdd++;
+// }
