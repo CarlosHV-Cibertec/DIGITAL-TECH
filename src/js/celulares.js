768484018f3e4e27f149265538c121d7e28c4f6c
EstefanyR24 <i202124794@cibertec.edu.pe>
2023-06-20 00:36:49 -0500

    a

--- src/js/celulares.js
+++ src/js/celulares.js
@@ -97,26 +97,308 @@ const celulares = [
       id: "ce10"
     },
   ];
+
+  //Array de objetos para compus
+  const computadoras = [
+    {
+      marca: "LOLJ",
+      modelo: "php 3",
+      imagen: "./src/assets/img/pc1.jpg",
+      precio: 4500.99,
+      id: "co01"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/pc2.jfif",
+      precio: 999.99,
+      id: "co02"    
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc3.jfif",
+      precio: 1459.99,
+      id: "co03"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc4.jfif",
+      precio: 1459.99,
+      id: "co04"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/pc1.jpg",
+      precio: 999.99,
+      id: "co05"      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc2.jfif",
+      precio: 1459.99,
+      id: "co06"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc3.jfif",
+      precio: 1459.99,
+      id: "co07"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc4.jfif",
+      precio: 1459.99,
+      id: "co08"
+      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc1.jpg",
+      precio: 1459.99,
+      id: "co09"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/pc2.jfif",
+      precio: 1459.99,
+      id: "co10"
+    },
+  ];
+
+  //Array de objetos para laptops
+  const laptops = [
+    {
+      marca: "LOLJ",
+      modelo: "php 3",
+      imagen: "./src/assets/img/laptop1.jpg",
+      precio: 4500.99,
+      id: "co01"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/laptop2.jfif",
+      precio: 999.99,
+      id: "co02"    
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop3.jpg",
+      precio: 1459.99,
+      id: "co03"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop4.jpg",
+      precio: 1459.99,
+      id: "co04"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/laptop5.jpg",
+      precio: 999.99,
+      id: "co05"      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop6.jpg",
+      precio: 1459.99,
+      id: "co06"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop7.jfif",
+      precio: 1459.99,
+      id: "co07"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop8.jfif",
+      precio: 1459.99,
+      id: "co08"
+      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop9.jfif",
+      precio: 1459.99,
+      id: "co09"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/laptop10.jfif",
+      precio: 1459.99,
+      id: "co10"
+    },
+  ];
+   
+  // Array de objetos para tablets
+  const tablets = [
+    {
+      marca: "LOLJ",
+      modelo: "php 3",
+      imagen: "./src/assets/img/tablet1.jfif",
+      precio: 4500.99,
+      id: "co01"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/tablet2.jpg",
+      precio: 999.99,
+      id: "co02"    
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet3.jfif",
+      precio: 1459.99,
+      id: "co03"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet2.jpg",
+      precio: 1459.99,
+      id: "co04"
+    },
+    {
+      marca: "Samsung",
+      modelo: "Galaxy S21",
+      imagen: "./src/assets/img/tablet1.jfif",
+      precio: 999.99,
+      id: "co05"      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet3.jfif",
+      precio: 1459.99,
+      id: "co06"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet7.jpg",
+      precio: 1459.99,
+      id: "co07"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet1.jfif",
+      precio: 1459.99,
+      id: "co08"
+      
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet2.jpg",
+      precio: 1459.99,
+      id: "co09"
+    },
+    {
+      marca: "Apple",
+      modelo: "iPhone 12",
+      imagen: "./src/assets/img/tablet3.jfif",
+      precio: 1459.99,
+      id: "co10"
+    },
+  ];
   
-  // Función para generar el HTML de los celulares
-  function mostrarCelulares() {
-    const celularesContainer = document.getElementById("celulares");
-    celularesContainer.innerHTML = "";
+     // Función para generar el HTML de las celularess
+     function mostrarCelulares() {
+      const celularesContainer = document.getElementById("celulares");
+      celularesContainer.innerHTML = "";
+    
+      celulares.forEach(celular => {
+   
+        const celularesHTML = `
+          <div class="celular">
+            <img src="${celular.imagen}" alt="${celular.marca} ${celular.modelo}">
+            <h3>${celular.marca} ${celular.modelo}</h3>
+            <p>Precio: $${celular.precio}</p>
+            <button onclick="agregarAlCarrito('${celular.imagen}','${celular.marca}', '${celular.modelo}', ${celular.precio})">Agregar al carrito</button>
+          </div>
+        `;
+        celularesContainer.innerHTML += celularesHTML;
+      });
+    }
+
+  // Función para generar el HTML de los tablets
+  function mostrarTablets() {
+    const tabletsContainer = document.getElementById("tablets");
+    tabletsContainer.innerHTML = "";
   
-    celulares.forEach(celular => {
+    tablets.forEach(tablet => {
  
-      const celularHTML = `
-        <div class="celular">
-          <img src="${celular.imagen}" alt="${celular.marca} ${celular.modelo}">
-          <h3>${celular.marca} ${celular.modelo}</h3>
-          <p>Precio: $${celular.precio}</p>
-          <button onclick="agregarAlCarrito('${celular.imagen}','${celular.marca}', '${celular.modelo}', ${celular.precio})">Agregar al carrito</button>
+      const tabletsHTML = `
+        <div class="tablet">
+          <img src="${tablet.imagen}" alt="${tablet.marca} ${tablet.modelo}">
+          <h3>${tablet.marca} ${tablet.modelo}</h3>
+          <p>Precio: $${tablet.precio}</p>
+          <button onclick="agregarAlCarrito('${tablet.imagen}','${tablet.marca}', '${tablet.modelo}', ${tablet.precio})">Agregar al carrito</button>
         </div>
       `;
-      celularesContainer.innerHTML += celularHTML;
+      tabletsContainer.innerHTML += tabletsHTML;
     });
   }
-  
+
+  //Función para computadoras
+    function mostrarComputadoras() {
+      const computadorasContainer = document.getElementById("computadoras");
+      computadorasContainer.innerHTML = "";
+    
+      computadoras.forEach(computadora => {
+   
+        const computadorasHTML = `
+          <div class="computadora">
+            <img src="${computadora.imagen}" alt="${computadora.marca} ${computadora.modelo}">
+            <h3>${computadora.marca} ${computadora.modelo}</h3>
+            <p>Precio: $${computadora.precio}</p>
+            <button onclick="agregarAlCarrito('${computadora.imagen}','${computadora.marca}', '${computadora.modelo}', ${computadora.precio})">Agregar al carrito</button>
+          </div>
+        `;
+        computadorasContainer.innerHTML += computadorasHTML;
+      });
+    }
+
+    // Función para generar el HTML de las laptops
+    function mostrarLaptops() {
+      const laptopsContainer = document.getElementById("laptops");
+      laptopsContainer.innerHTML = "";
+    
+      laptops.forEach(laptop => {
+   
+        const laptopsHTML = `
+          <div class="laptop">
+            <img src="${laptop.imagen}" alt="${laptop.marca} ${laptop.modelo}">
+            <h3>${laptop.marca} ${laptop.modelo}</h3>
+            <p>Precio: $${laptop.precio}</p>
+            <button onclick="agregarAlCarrito('${laptop.imagen}','${laptop.marca}', '${laptop.modelo}', ${laptop.precio})">Agregar al carrito</button>
+          </div>
+        `;
+        laptopsContainer.innerHTML += laptopsHTML;
+      });
+    }
+
 
   // ! variables del carrito
   const prodSelecItm = document.getElementById("icnCar_itm");
@@ -234,7 +516,14 @@ function eliminarDelCarrito(id, precio) {
 
 
 
-
   //mostrarCelulares al cargar la página
   mostrarCelulares();
+
+  //mostrarTablets al cargar la página
+  mostrarTablets();
   
+  //mostrarComputadoras al cargar la página
+  mostrarComputadoras();
+  
+  //mostrarComputadoras al cargar la página
+  mostrarLaptops();
