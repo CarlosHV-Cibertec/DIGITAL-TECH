40bcf3f8623833a9258d75a103f63cdf30f28ec2
luispal7030 <i202224764@cibertec.edu.pe>
2023-06-29 19:27:43 -0500

    as

--- src/js/celulares.js
+++ src/js/celulares.js
@@ -375,7 +375,7 @@ function mostrarProducto(identifier, arrayproductos) {
             <img src="${celular.imagen}" alt="${celular.marca} ${celular.modelo}">
             <h3>${celular.marca} ${celular.modelo}</h3>
             <p>Precio: $${celular.precio}</p>
-            <button onclick="agregarAlCarrito('${celular.imagen}','${celular.marca}', '${celular.modelo}', ${celular.precio})">Agregar al carrito</button>
+            <button onclick="agregarAlCarrito('${celular.imagen}','${celular.marca}', '${celular.modelo}', ${celular.precio})">Agregar al carrito </button>
           </div>
         `;
     //--colocara los productos mediante innerHTML
